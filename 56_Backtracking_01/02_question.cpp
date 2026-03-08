655f0413d3a7d6de475c08157a479c0b54387fb1
ayush931 <ayushkumar9315983@gmail.com>
2026-03-08 15:39:22 +0530

    Backtracking 01

---
+++ 56_Backtracking_01/02_question.cpp
@@ -0,0 +1,7 @@
+/**
+ * Rat in a Maze
+
+ * A maze is a N * N binary matrix of blocks where the upper left block is known as the source block, and the lower rightmost back is known as the Destination block. If we consider the maze, the maze[0][0] is the source, and maze[N - 1][N - 1] is the destination. Our main task is to reach the destination from the source. We have considered a rat as a character that can move left, right, up and down.
+
+ * In the maze matrix, a few dead blocks will be denoted by 0 and active blocks will be denoted by 1. A rat can move only in the active blocks.
+ */
