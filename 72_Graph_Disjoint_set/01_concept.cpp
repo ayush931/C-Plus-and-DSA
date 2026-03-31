c9f54bb5c169176010fffa4737e7f06c5f5e1056
ayush931 <ayushkumar9315983@gmail.com>
2026-03-31 18:39:40 +0530

    Disjoint graph

---
+++ 72_Graph_Disjoint_set/01_concept.cpp
@@ -0,0 +1,13 @@
+/**
+ * Leader / parent of the group:- To uniquely identify a group, we will pick any element from the group & make it representative / leader / parent of the group
+ * 
+ * 1. Union(a, b):- adds the group where element b belongs to the group where the elements a belongs or vice-versa.
+ * 2. find(x) / get(x):- this will be used to find which group x belongs to, we will return the parent of the group that x belongs to
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello world";
+}
